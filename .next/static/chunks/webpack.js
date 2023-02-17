fb6e9e55b21e8aaeffb4b9dd34c7e3711950267f
ItaloCobains <italobrandao.contato@gmail.com>
2023-02-17 00:07:31 -0300

    feat: Tasks

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2dd6c68b7d724263"; }
+/******/ 		__webpack_require__.h = function() { return "18d7dfed931ffb1e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -12,139 +12,72 @@ import { setupAPIClient } from '../../services/api'
 import Modal from 'react-modal'
 import { ModalOrder } from '../../components/ModalOrder'
 
-type OrderProps = {
+type TaskProps = {
     id: string
-    table: string | number
-    status: boolean
-    draft: boolean
-    name: string | null
+    title: string
+    description: string
+    done: boolean
+    creted_at: Date
+    updated_at: Date
+    user_id: string
 }
 
 interface HomeProps {
-    orders: OrderProps[]
+    tasks: TaskProps[]
 }
 
-export type OrderItemProps = {
-    id: string
-    amount: number
-    order_id: string
-    product_id: string
-    product: {
-        id: string
-        name: string
-        description: string
-        price: string
-        banner: string
-    }
-    order: {
-        id: string
-        table: string | number
-        status: boolean
-        name: string | null
-    }
-}
-
-export default function Dashboard({ orders }: HomeProps) {
-    const [orderList, setOrderList] = useState(orders || [])
-
-    const [modalItem, setModalItem] = useState<OrderItemProps[]>()
-    const [modalVisible, setModalVisible] = useState(false)
+export default function Dashboard({ tasks }: HomeProps) {
+    const [taskList, setTaskList] = useState<TaskProps[]>(tasks || [])
+    const [modalVisible, setModalVisible] = useState<boolean>(false)
 
     function handleCloseModal() {
         setModalVisible(false)
     }
 
-    async function handleModalOpenView(id: string) {
+    async function handleRefreshTasks() {
         const apiClient = setupAPIClient()
 
-        const response = await apiClient.get('/order/detail', {
-            params: {
-                order_id: id,
-            }
-        })
+        const response = await apiClient.get('/tasks/all')
 
-        setModalItem(response.data)
+        setTaskList(response.data)
         setModalVisible(true)
     }
 
-    async function handleFinishItem(id: string) {
-        const apiClient = setupAPIClient()
-        await apiClient.put('/order/finish', {
-            order_id: id,
-        })
-
-        const response = await apiClient.get('/orders');
-
-        setOrderList(response.data)
-        setModalVisible(false)
-    }
-
-    async function handleRefreshOrders() {
-        const apiClient = setupAPIClient()
-        const response = await apiClient.get('/orders')
-        setOrderList(response.data)
-    }
-
     Modal.setAppElement('#__next')
-
     return (
         <>
             <Head>
-                <title>Painel - Projeto Pizzaria</title>
+                <title>Ferrinox - Lista De Tarefas</title>
             </Head>
             <div>
                 <Header />
 
                 <main className={styles.container}>
                     <div className={styles.containerHeader}>
-                        <h1>Últimos pedidos</h1>
-                        <button onClick={handleRefreshOrders}>
+                        <h1>Últimas Tarefas</h1>
+                        <button onClick={handleRefreshTasks}>
                             <FiRefreshCcw size={25} color='#3fffa3' />
                         </button>
                     </div>
-
                     <article className={styles.listOrders}>
-
-                        {orderList.length === 0 && (
+                        {taskList.length === 0 && (
                             <span className={styles.emptyList}>
-                                Nenhum pedido aberto foi encontrado...
+                                Nenhuma tarefa aberto foi encontrado...
                             </span>
                         )}
-
-                        {orderList.map(item => (
-                            <section className={styles.orderItem} key={item.id}>
-                                <button onClick={() => handleModalOpenView(item.id)}>
-                                    <div className={styles.tag}></div>
-                                    <span>Mesa {item.table}</span>
-                                </button>
-                            </section>
-                        ))}
                     </article>
                 </main>
-
-                {modalVisible && (
-                    <ModalOrder
-                        isOpen={modalVisible}
-                        onRequestClose={handleCloseModal}
-                        order={modalItem}
-                        handleFinishOrder={handleFinishItem}
-                    />
-                )}
             </div>
         </>
     )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-    // const apiClient = setupAPIClient(ctx)
-    // const response = await apiClient.get('/orders')
-
-    // return {
-    //     props: {
-    //         orders: response.data
-    //     }
-    // }
+    const apiClient = setupAPIClient(ctx)
+    const response = apiClient.get('/tasks/all')
     return {
-        props: {}
+        props: {
+            tasks: (await response).data
+        }
     }
 })
