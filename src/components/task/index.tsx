018959cb3d0b9ed7cc8e5ad3274a295c5875ecb3
ItaloCobains <italobrandao.contato@gmail.com>
2023-02-22 12:27:04 -0300

    feat: button done

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,35 +1,48 @@
-import { AiFillEdit } from 'react-icons/ai';
-import { MdDelete } from 'react-icons/md';
-import { TaskProps } from '../../pages/dashboard';
-import { api } from '../../services/apiClient';
-import { formatDate } from '../../utils/formatDate';
-import styles from './styles.module.scss';
+import { AiFillEdit } from "react-icons/ai";
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
+import { TaskProps } from "../../pages/dashboard";
+import { api } from "../../services/apiClient";
+import { formatDate } from "../../utils/formatDate";
+import styles from "./styles.module.scss";
 
 interface TaskPropsComponent {
-    task: TaskProps
-    onFetchRemove: () => void
+  task: TaskProps;
+  onFetchRemove: () => void;
 }
 
-const Task = ({ task ,onFetchRemove }: TaskPropsComponent) => {
-    const removeTask = async () => {
-        if (window.confirm('Deseja realmente excluir esta tarefa?')) {
-            await api.delete(`/tasks/${task._id}`)
-            onFetchRemove()
-        }
+const Task = ({ task, onFetchRemove }: TaskPropsComponent) => {
+  const changeDone = async () => {
+    await api.put(`/tasks/done/${task._id}`, {
+      done: !task.done,
+    });
+    onFetchRemove();
+  };
+  const removeTask = async () => {
+    if (window.confirm("Deseja realmente excluir esta tarefa?")) {
+      await api.delete(`/tasks/${task._id}`);
+      onFetchRemove();
     }
-    return (
-        <div className={`${styles.task} ${task.done ? styles.done : ''}`}>
-            <h3>{task.title}</h3>
-            <h5>{task.description}</h5>
-            <MdDelete onClick={removeTask}  className={styles.delete} size={20}  />
-            <AiFillEdit className={styles.edit} size={20}  />
-            <div className={styles.metadata}>
-                <span>Criado em: {formatDate(task.created_at)}</span>
-                <span>Atualizado em: {formatDate(task.updated_at)}</span>
-                <span>Criado por: {task.user_name}</span>
-            </div>
-        </div>
-    );
-}
+  };
+  return (
+    <div className={`${styles.task} ${task.done ? styles.done : ""}`}>
+      <h3>{task.title}</h3>
+      <h5>{task.description}</h5>
+      <IoCheckmarkDoneCircleSharp
+        onClick={changeDone}
+        className={styles.ChangeDone}
+        size={20}
+        color={task.done ? "#2ECC40" : "#FF4136"}
+      />
+      <MdDelete onClick={removeTask} className={styles.delete} size={20} />
+      <AiFillEdit className={styles.edit} size={20} />
+      <div className={styles.metadata}>
+        <span>Criado em: {formatDate(task.created_at)}</span>
+        <span>Atualizado em: {formatDate(task.updated_at)}</span>
+        <span>Criado por: {task.user_name}</span>
+      </div>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
--- src/components/tasklist/index.tsx
+++ src/components/tasklist/index.tsx
@@ -1,21 +1,20 @@
-import { TaskProps } from '../../pages/dashboard'
-import Task from '../task'
-import styles from './styles.module.scss'
-
+import { TaskProps } from "../../pages/dashboard";
+import Task from "../task";
+import styles from "./styles.module.scss";
 
 type TaskListProps = {
-    tasks: TaskProps[]
-    onFetchData: () => void
-}
+  tasks: TaskProps[];
+  onFetchData: () => void;
+};
 
 const TaskList = ({ tasks, onFetchData }: TaskListProps) => {
-    return (
-        <div className={styles.taskList}>
-            {tasks.map((task, index) => (
-                <Task onFetchRemove={onFetchData} key={task._id}  task={task} />
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className={styles.taskList}>
+      {tasks.map((task, index) => (
+        <Task onFetchRemove={onFetchData} key={task._id} task={task} />
+      ))}
+    </div>
+  );
+};
 
 export default TaskList;
